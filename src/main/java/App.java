68a1c757add94b0554b5b0fbb3090f2aebc170db
xuan.yin <xuan.yin@shuyun.com>
2018-03-29 11:18:36 +0800

    all changed

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+/**
+ * DemoApp
+ *
+ * @author xuan.yin
+ * @since 2018/3/29 11:17
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("SubModule");
+
+    }
+}
